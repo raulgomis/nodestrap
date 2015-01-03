60d21ac81ec4d815f0c654d4e58f61eef3d074d0
Raul Gomis <raulgomis@hotmail.com>
2015-01-03 23:11:32 +0100

    ADDED Initial server infrastructure

---
+++ server/app.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var serveStatic = require('serve-static');
+
+var debug = require('debug')('beerstrap-nodejs');
+
+// Application requires
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var app = express();
+
+// set the view engine to ejs
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(serveStatic(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+
+if(app.get('env') == 'development') {
+    // error handlers
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+else {
+    // error handlers
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
+
+app.set('port', process.env.PORT || 4000);
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+});
+
